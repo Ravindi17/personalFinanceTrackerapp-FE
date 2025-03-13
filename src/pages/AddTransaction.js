787792921c5e305511817f501a04923120f46533
Ravindi Welagedara <ravindiwelagedara@gmail.com>
2025-03-13 13:41:42 +0530

    added the trasaction addition page

---
+++ src/pages/AddTransaction.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionForm from "../components/TransactionForm";
+
+const AddTransaction = () => {
+  return (
+    <div>
+      <h1>Add Transaction</h1>
+      <TransactionForm />
+    </div>
+  );
+};
+
+export default AddTransaction;
